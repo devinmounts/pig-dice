dfcea399e30b2a7a11c9ca302f492106f96ee051
Devin Mounts and Matt Smith <matt@>
2018-06-13 09:55:39 -0700

    push diceResult to empty array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-function player(totalPlayer1, totalPlayer2){
-  this.player1 = []
-  this.player2 = []
-}
-
-function gameBoard(player, roundTotal) {
-
-}
+var round = []
 
 
-function diceRoll() {
+function diceRoll(diceResult) {
     var diceResult = Math.floor((Math.random() * 6) + 1);
     console.log(diceResult);
+    round.push(diceResult);
+    console.log(round);
+}
+
+
+function gameBoard(player, roundTotal) {
+player = player;
+roundTotal = [];
+console.log(gameBoard.roundTotal);
+}
+
+function player(totalPlayer1, totalPlayer2){
+  this.player1 = [];
+  this.player2 = [];
 }
